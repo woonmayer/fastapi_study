0db81175358180c4f43454f7bacac29895b814ba
LeeSeungWoon <s3wn.code@gmail.com>
2022-06-22 19:25:23 +0900

    Add simple token auth

--- day4/main.py
+++ day4/main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from fastapi.param_functions import Depends
+from fastapi.security import OAuth2PasswordBearer
 
 app =  FastAPI()
 
 @app.get('/')
 def read_main():
+    ...
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+@app.get('/auth_needed')
+def auth_needed(response: Response, token: str = Depends(oauth2_scheme)):
     ...
--- day4/test_main.py
+++ day4/test_main.py
@@ -1,6 +1,15 @@
 from fastapi.testclient import TestClient
 import main
 
+
 def test_read_main():
     client = TestClient(main.app)
     assert client.get('/').status_code == 200
+
+
+def test_read_auth_needed():
+    client = TestClient(main.app)
+    assert client.get('/auth_needed').status_code == 401
+    assert client.get('/auth_needed', headers={
+        'Authorization': f'Bearer TEST_TOKEN'
+    }).status_code == 200
